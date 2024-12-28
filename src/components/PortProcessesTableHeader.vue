856c7291c263d82e59a4a4e077f025c047152580
LazyDoomSlayer <lazydoomslayer@gmail.com>
2024-12-28 17:20:09 +0100

    feat: add basic component structure for list display

---
+++ src/components/PortProcessesTableHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="process-table-header">
+    <div class="process-table-header__pid">PID</div>
+    <div class="process-table-header__port">Port</div>
+    <div class="process-table-header__process-name">Process Name</div>
+    <div class="process-table-header__process-path">Process Path</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  console.log('hello from ehader');
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/abstracts/_mixins.scss' as mixins;
+.process-table-header {
+  @include mixins.flex-display;
+
+  flex-grow: 1;
+  border: 2px solid #3e3e3e;
+  padding: 4px;
+  border-radius: 4px;
+  > div {
+    height: 32px;
+    padding: 0px 6px;
+
+    @include mixins.flex-display;
+
+    align-items: center;
+  }
+
+  &__pid {
+    width: 10%;
+  }
+  &__port {
+    width: 10%;
+  }
+  &__process-name {
+    width: 30%;
+  }
+  &__process-path {
+    width: 40%;
+  }
+  &__actions {
+    width: 10%;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
-import VirtualList from '@/components/virtual/VirtualList.vue';
-
 import { onMounted, ref } from 'vue';
 import { invoke } from '@tauri-apps/api/core';
 
-import { EScrollBehavior } from '@/types/virtual-list.types';
 import type { TPortProcessList } from '@/types';
+import PortProcessesTable from '@/components/PortProcessesTable.vue';
 
 const ports = ref<TPortProcessList>([]);
 
@@ -22,36 +20,8 @@ async function getPorts(): Promise<void> {
 onMounted(() => {
   getPorts();
 });
-
-const searchModel = ref<number>(100);
-const virtualListRef = ref();
 </script>
 
 <template>
-  <input
-    type="number"
-    v-model="searchModel"
-    label="search"
-    placeholder="search"
-  />
-  <button @click.left="getPorts">fetch again</button>
-  <button @click.left="virtualListRef.goToElementWithIndex(searchModel)">
-    Search
-  </button>
-  <button @click.left="virtualListRef.goToBottom">To Bottom</button>
-  <button @click.left="virtualListRef.goToStart">To Top</button>
-
-  <div style="height: 900px">
-    <VirtualList
-      :list="ports"
-      ref="virtualListRef"
-      :item-height="21"
-      :scroll-behavior="EScrollBehavior.SMOOTH"
-    >
-      <template #item="{ item: port }">
-        Port: {{ port.port }}, PID: {{ port.pid }}, Process:
-        {{ port.process_name }}, Procs.Path: {{ port.process_path }}
-      </template>
-    </VirtualList>
-  </div>
+  <PortProcessesTable :list="ports" />
 </template>
