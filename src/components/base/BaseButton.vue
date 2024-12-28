62db5198e81de3ddd55378df606fee8ca55e1bfc
LazyDoomSlayer <lazydoomslayer@gmail.com>
2024-12-28 22:09:05 +0100

    feat: add loading and disabled state

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,15 +1,24 @@
 <template>
   <button
-    @click.left="emit('leftClicked', $event)"
-    @click.right="emit('rightClicked', $event)"
+    @click.left="leftClicked($event)"
+    @click.right="rightClicked($event)"
     :style="computedStyleObject"
+    :class="{ 'disabled-state': componentProps.isDisabled }"
   >
     <slot name="prepend">
       <span v-if="componentProps.prependIcon">
         {{ componentProps.prependIcon }}
       </span>
     </slot>
-    <slot name="content"> {{ text }} </slot>
+
+    <CircleSpinner
+      v-if="componentProps.isLoading"
+      :stroke-width="4"
+      :size="20"
+    />
+
+    <slot v-else name="content"> {{ text }} </slot>
+
     <slot name="append">
       <span v-if="componentProps.appendIcon">
         {{ componentProps.appendIcon }}
@@ -20,9 +29,11 @@
 
 <script setup lang="ts">
 import { computed, type CSSProperties } from 'vue';
+import CircleSpinner from '../loaders/CircleSpinner.vue';
 
 interface IProps {
   isDisabled?: boolean;
+  isLoading?: boolean;
   prependIcon?: string;
   appendIcon?: string;
   text: string;
@@ -37,9 +48,21 @@ const emit = defineEmits<{
 
 const computedStyleObject = computed<CSSProperties>(() => {
   return {
-    cursor: componentProps.isDisabled ? 'disabled' : 'pointer',
+    cursor: componentProps.isDisabled ? 'default' : 'pointer',
   };
 });
+
+function leftClicked(event: MouseEvent) {
+  if (componentProps.isLoading || componentProps.isDisabled) return;
+
+  emit('leftClicked', event);
+}
+
+function rightClicked(event: MouseEvent) {
+  if (componentProps.isLoading || componentProps.isDisabled) return;
+
+  emit('rightClicked', event);
+}
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +70,8 @@ const computedStyleObject = computed<CSSProperties>(() => {
 
 button {
   @include mixins.flex-display;
+  justify-content: center;
+
   align-items: center;
 }
 </style>
---
+++ src/components/loaders/CircleSpinner.vue
@@ -0,0 +1,88 @@
+<template>
+  <svg
+    class="spinner"
+    :width="size"
+    :height="size"
+    :viewBox="`0 0 ${size} ${size}`"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      class="path"
+      fill="none"
+      :stroke-width="strokeWidth"
+      stroke-linecap="round"
+      :cx="size / 2"
+      :cy="size / 2"
+      :r="(size - strokeWidth) / 2"
+    ></circle>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+
+withDefaults(
+  defineProps<{
+    size?: number;
+    strokeWidth?: number;
+  }>(),
+  {
+    size: 50,
+    strokeWidth: 5,
+  },
+);
+</script>
+
+<style scoped lang="scss">
+.spinner {
+  animation: rotate 1s linear infinite;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.path {
+  stroke: #3498db;
+  stroke-dasharray: 150, 200;
+  stroke-dashoffset: -10;
+  animation:
+    dash 1.5s ease-in-out infinite,
+    color 6s ease-in-out infinite;
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 200;
+    stroke-dashoffset: -40;
+  }
+  100% {
+    stroke-dasharray: 90, 200;
+    stroke-dashoffset: -120;
+  }
+}
+
+@keyframes color {
+  0% {
+    stroke: #3498db;
+  }
+  25% {
+    stroke: #e74c3c;
+  }
+  50% {
+    stroke: #f1c40f;
+  }
+  75% {
+    stroke: #2ecc71;
+  }
+  100% {
+    stroke: #3498db;
+  }
+}
+</style>
