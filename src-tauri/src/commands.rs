d8a9dc570e77ebf5a9d2a988ca8792f296bf07eb
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-23 09:47:30 +0200

    fix: Add missing struct import in file.

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,4 +1,4 @@
-use crate::common::{KillProcessResponse, PortInfo, ProcessInfoResponse};
+use crate::common::{KillProcessResponse, PortInfo, ProcessInfoResponse, ProcessInfo};
 use crate::state::AppState;
 
 use std::sync::{Arc, Mutex};
