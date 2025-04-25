c67561f3f5059fa8fde6226237d3d784425f1bc0
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-25 18:19:30 +0200

    feat(styles): Change app  font family.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 // Fonts
-import '@fontsource/jetbrains-mono/400.css';
-import '@fontsource/jetbrains-mono/800.css';
+import '@fontsource/funnel-sans';
 
 import 'material-icons/iconfont/outlined.css';
 import 'material-symbols/outlined.css';
