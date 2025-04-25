cf9282b4f3714a349efffd47bbdb145bbfc8feab
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-26 00:04:52 +0200

    chore(components): Remove vue dev tools temporary.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   clearScreen: false,
@@ -19,13 +18,13 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
+          protocol: 'ws',
           host,
           port: 1421,
         }
       : undefined,
     watch: {
-      ignored: ["**/src-tauri/**"],
+      ignored: ['**/src-tauri/**'],
     },
   },
 }));
