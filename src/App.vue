60fed59c608716236203e34d89e1bd1a73243313
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-28 15:41:37 +0200

    feat(root): Add listener to remove browser context menu from build.

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,20 @@ function handleBlur() {
   applicationStore.isApplicationFocused = false;
 }
 
+function handleContextMenu(event: MouseEvent) {
+  event.preventDefault();
+}
+
 onMounted(() => {
   window.addEventListener('focus', handleFocus);
   window.addEventListener('blur', handleBlur);
+  window.addEventListener('contextmenu', handleContextMenu);
 });
 
 onUnmounted(() => {
   window.removeEventListener('focus', handleFocus);
   window.removeEventListener('blur', handleBlur);
+  window.removeEventListener('contextmenu', handleContextMenu);
 });
 </script>
 
